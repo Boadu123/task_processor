ddd70ce5ed59819f6b1f24bcfbed210615bf34b4
Benedict Boadu-Boateng <bboaduboateng2000@gmail.com>
2025-06-19 10:19:46 +0000

    feat: implement multithreaded Consumer and Producer using Runnable

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,35 @@
 package com.example;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import com.example.models.Consumer;
+import com.example.models.Producer;
+import com.example.models.Task;
+
+import java.util.concurrent.PriorityBlockingQueue;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws InterruptedException {
+
+        Producer producer = new Producer("Washer", 2, true);
+        Thread t1 = new Thread(producer);
+
+        Producer producer1 = new Producer("Machine", 2, false);
+        Thread t2 = new Thread(producer1);
+
+        ExecutorService consumerPool = Executors.newFixedThreadPool(2);
+
+        consumerPool.submit(new Consumer());
+        consumerPool.submit(new Consumer());
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+        consumerPool.shutdown();
     }
 }
+
